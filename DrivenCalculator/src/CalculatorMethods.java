2ea8bc7d04f746820426eed82d0d626d5f736ab2
desry <77392565+DESRYMATJOALA@users.noreply.github.com>
2022-01-06 04:10:19 +0200

    Add files via upload

---
+++ DrivenCalculator/src/CalculatorMethods.java
@@ -0,0 +1,6 @@
+public interface CalculatorMethods {
+    int addition(int a,int b);
+    int subtraction(int a,int b);
+    int multiplecation(int a,int b);
+    int division(int a,int b);
+}
---
+++ DrivenCalculator/src/Operaions.java
@@ -0,0 +1,21 @@
+public class Operaions implements CalculatorMethods{
+    @Override
+    public int addition(int a, int b) {
+        return a+b;
+    }
+
+    @Override
+    public int subtraction(int a, int b) {
+        return a-b;
+    }
+
+    @Override
+    public int multiplecation(int a, int b) {
+        return a*b;
+    }
+
+    @Override
+    public int division(int a, int b) {
+        return a/b;
+    }
+}
